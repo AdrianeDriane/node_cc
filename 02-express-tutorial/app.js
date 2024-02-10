bfbe803e4d85f44184fb4bf317782b5fcad7ab02
AdrianeDriane <dilaoaparre@gmail.com>
2024-02-10 14:31:29 +0800

    intro to middleware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,63 +1,24 @@
 const express = require('express');
 const app = express();
-const { products } = require('./data.js');
+const logger = require('./logger.js');
+// req => middleware => res
+
+app.use(logger);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Home Page</h1><a href="/api/products">Products</a>');
+  res.send('Home');
+});
+
+app.get('/about', (req, res) => {
+  res.send('About');
 });
 
 app.get('/api/products', (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-
-  res.json(newProducts);
+  res.send('Products');
 });
 
-app.get('/api/products/:productID', (req, res) => {
-  const productID = req.params.productID;
-
-  const singleProduct = products.find(
-    (product) => product.id === parseInt(productID)
-  );
-
-  if (singleProduct) {
-    res.json(singleProduct);
-  } else {
-    res.status(404).send(`<h1>Error 404: Product ${productID} not found.</h1>`);
-  }
-});
-
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-  res.send(req.params);
-});
-
-app.get('/api/v1/query', (req, res) => {
-  const { search, limit } = req.query;
-  let sortedProducts = [...products];
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search);
-    });
-  }
-
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-  }
-
-  if (sortedProducts < 1) {
-    // res.status(200).send('No products matched your search.');
-    res.status(200).json({ success: true, data: [] });
-    return;
-  }
-
-  res.status(200).json(sortedProducts);
-});
-
-app.all('*', (req, res) => {
-  res.status(404).send('<h1>Error 404: Page not found.</h1>');
+app.get('/api/items', (req, res) => {
+  res.send('Items');
 });
 
 app.listen(5000, () => {
--- 02-express-tutorial/final/07-params-query.js
+++ 02-express-tutorial/final/07-params-query.js
@@ -1,58 +1,65 @@
-const express = require('express')
-const app = express()
-const { products } = require('./data')
+const express = require('express');
+const app = express();
+const { products } = require('./data.js');
 
 app.get('/', (req, res) => {
-  res.send('<h1> Home Page</h1><a href="/api/products">products</a>')
-})
+  res.send('<h1>Home Page</h1><a href="/api/products">Products</a>');
+});
+
 app.get('/api/products', (req, res) => {
   const newProducts = products.map((product) => {
-    const { id, name, image } = product
-    return { id, name, image }
-  })
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
 
-  res.json(newProducts)
-})
+  res.json(newProducts);
+});
+
 app.get('/api/products/:productID', (req, res) => {
-  // console.log(req)
-  // console.log(req.params)
-  const { productID } = req.params
+  const productID = req.params.productID;
 
   const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  )
-  if (!singleProduct) {
-    return res.status(404).send('Product Does Not Exist')
-  }
+    (product) => product.id === parseInt(productID)
+  );
 
-  return res.json(singleProduct)
-})
+  if (singleProduct) {
+    res.json(singleProduct);
+  } else {
+    res.status(404).send(`<h1>Error 404: Product ${productID} not found.</h1>`);
+  }
+});
 
 app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-  console.log(req.params)
-  res.send('hello world')
-})
+  res.send(req.params);
+});
 
 app.get('/api/v1/query', (req, res) => {
-  // console.log(req.query)
-  const { search, limit } = req.query
-  let sortedProducts = [...products]
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
 
   if (search) {
     sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search)
-    })
+      return product.name.startsWith(search);
+    });
   }
+
   if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit))
+    sortedProducts = sortedProducts.slice(0, Number(limit));
   }
-  if (sortedProducts.length < 1) {
-    // res.status(200).send('no products matched your search');
-    return res.status(200).json({ sucess: true, data: [] })
+
+  if (sortedProducts < 1) {
+    // res.status(200).send('No products matched your search.');
+    res.status(200).json({ success: true, data: [] });
+    return;
   }
-  res.status(200).json(sortedProducts)
-})
+
+  res.status(200).json(sortedProducts);
+});
+
+app.all('*', (req, res) => {
+  res.status(404).send('<h1>Error 404: Page not found.</h1>');
+});
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
-})
+  console.log('Server is listening on port 5000...');
+});
