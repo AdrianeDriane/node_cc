5e95e6fa565de7a4fbca57814f618ff5801f9157
AdrianeDriane <dilaoaparre@gmail.com>
2024-02-10 15:01:01 +0800

    multiple middleware functions

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const logger = require('./logger.js');
+const authorize = require('./authorize.js');
 // req => middleware => res
+// app.use([logger, authorize]);
+// app.use(express.static('./public'));
 
-app.use(logger);
+app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
   res.send('Home');
@@ -18,6 +22,7 @@ app.get('/api/products', (req, res) => {
 });
 
 app.get('/api/items', (req, res) => {
+  console.log(req.user);
   res.send('Items');
 });
 
