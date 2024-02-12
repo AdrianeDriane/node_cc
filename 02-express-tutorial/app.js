4287c02df33820a301e5bb333a9458037afb9532
AdrianeDriane <dilaoaparre@gmail.com>
2024-02-12 15:16:16 +0800

    PUT method

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-let { people } = require('./data.js');
+let { people, updatePeople } = require('./data.js');
 
 // static assets
 app.use(express.static('./methods-public'));
@@ -43,6 +43,34 @@ app.post('/login', (req, res) => {
 
   res.status(401).send('Please provide credentials!');
   // res.send(`Welcome, ${req.body.name}`);
+});
+
+app.put('/api/people/:id', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).send({
+      success: false,
+      msg: 'Please provide a value.',
+    });
+  }
+
+  const foundPerson = people.find((person) => person.id.toString() === id);
+
+  if (!foundPerson) {
+    return res.status(404).send({
+      success: false,
+      msg: 'ID does not match any user in the system.',
+    });
+  }
+
+  foundPerson.name = name;
+
+  res.status(200).send({
+    success: true,
+    data: people,
+  });
 });
 
 app.listen(5000, () => {
